3ce2df7d845c7a00377988dba73950d255460b39
Pepsiwiz <63413460+Pepsiwiz@users.noreply.github.com>
2020-06-03 19:35:25 +0300

    working chat

--- index.js
+++ index.js
@@ -8,3 +8,26 @@ server.listen(3000);
 app.get('/', function (request, respons) {
   respons.sendFile(__dirname + '/chat.html')
 });
+
+users = [];
+connections = [];
+
+io.sockets.on('connection', function(socket) {
+	console.log("Успешное соединение");
+
+	connections.push(socket);
+
+
+	socket.on('disconnect', function(data) {
+
+		connections.splice(connections.indexOf(socket), 1);
+		console.log("Отключились");
+	});
+
+
+	socket.on('send mess', function(data) {
+	
+		io.sockets.emit('add mess', {mess: data.mess, name: data.name, className: data.className});
+	});
+
+});
